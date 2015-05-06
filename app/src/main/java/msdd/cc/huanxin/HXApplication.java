564c1f72deadb7b5175ff6aac7fe9086059cb618
jpcq00x <jpcq_00x@sina.com>
2015-05-06 10:23:06 +0800

    第一次Commit这个文件

---
+++ app/src/main/java/msdd/cc/huanxin/HXApplication.java
@@ -0,0 +1,12 @@
+package msdd.cc.huanxin;
+
+import android.app.Application;
+
+/**
+ * @author pangcq    jpcq_00x@sina.com
+ * @Description: (这里用一句话描述这个类的作用)
+ * @date 2015/5/6 10:03
+ */
+public class HXApplication extends Application{
+
+}
