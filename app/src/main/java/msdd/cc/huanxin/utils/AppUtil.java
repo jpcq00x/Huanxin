52c26a94a279184462f4d8261f3ca4eb70ac204e
jpcq00x <jpcq_00x@sina.com>
2015-05-06 10:48:11 +0800

    第一次Commit这个文件

---
+++ app/src/main/java/msdd/cc/huanxin/utils/AppUtil.java
@@ -0,0 +1,15 @@
+package msdd.cc.huanxin.utils;
+
+import java.util.Date;
+
+/**
+ * @author pangcq    jpcq_00x@sina.com
+ * @Description: (这里用一句话描述这个类的作用)
+ * @date 2015/5/6 10:04
+ */
+public class AppUtil {
+    public static String date2string(Date date,String format){
+
+        return  null;
+    }
+}
