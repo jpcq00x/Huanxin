5c32f88f8c8a204664ed7c1fcb08f0b3e7da7730
jpcq00x <jpcq_00x@sina.com>
2015-05-06 20:09:36 +0800

    eeeee

--- app/src/main/java/msdd/cc/huanxin/domain/User.java
+++ app/src/main/java/msdd/cc/huanxin/domain/User.java
@@ -3,7 +3,7 @@ package msdd.cc.huanxin.domain;
 /**
  * @author pangcq    jpcq_00x@sina.com
  * @Description: (这里用一句话描述这个类的作用)
- * @date 2015/5/6 12:56
+ * @date 2015/5/6 12:56eeeeee
  */
 public class User {
     private String userId;
